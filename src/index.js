5d971475e4b56e659ccc16ff500d847c998f0eba
athena272 <guilhermera272@gmail.com>
2024-11-03 18:36:25 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs') // file system
 const path = require('path');
+const utils = require('./utils')
 
 const filesPathVector = [
     path.join(__dirname, '../files/texto-aprendizado.txt'),
@@ -8,25 +9,8 @@ const filesPathVector = [
 ];
 const link = filesPathVector[2]
 
-function checkDuplicateWords(text) {
-    const wordsList = text.split(' ')
-    const result = {}
-    // object[property] = value
-    wordsList.forEach(word => {
-        result[word] = (result[word] || 0) + 1
-    });
-
-    return result
-}
-
-function breakParagraphs(text) {
-    const paragraphs =  text.toLowerCase().split('\n')
-    const cont = paragraphs.map((paragraph) => checkDuplicateWords(paragraph))
-    console.log("🚀 ~ breakParagraphs ~ paragraphs:", cont)
-}
-
 fs.readFile(link, 'utf-8', (error, text) => {
-    breakParagraphs(text)
+    utils.breakParagraphs(text)
 })
 
 
