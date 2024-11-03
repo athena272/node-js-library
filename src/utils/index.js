667ccff8d5aa05a92a1f65a530e4fec62fa82b4f
athena272 <guilhermera272@gmail.com>
2024-11-03 18:36:27 -0300

    Create index.js

---
+++ src/utils/index.js
@@ -0,0 +1,20 @@
+function checkDuplicateWords(text) {
+    const wordsList = text.split(' ')
+    const result = {}
+    // object[property] = value
+    wordsList.forEach(word => {
+        result[word] = (result[word] || 0) + 1
+    });
+
+    return result
+}
+
+function breakParagraphs(text) {
+    const paragraphs =  text.toLowerCase().split('\n')
+    const cont = paragraphs.map((paragraph) => checkDuplicateWords(paragraph))
+    console.log("🚀 ~ breakParagraphs ~ paragraphs:", cont)
+}
+
+module.exports = {
+    breakParagraphs
+}
