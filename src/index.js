af28ba7edda503c75d58da2d8e9256eeeb49ece5
athena272 <guilhermera272@gmail.com>
2024-11-04 01:40:39 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,17 +9,29 @@ const filesPathVector = [
     path.join(__dirname, '../files/texto-web.txt'),
     path.join(__dirname, '../files/empty.txt'),
 ];
-const link = filesPathVector[3]
+const link = filesPathVector[2]
 
 fs.readFile(link, 'utf-8', (error, text) => {
     try {
         if (error || !text) throw error
 
-        breakParagraphs(text)
+        const result = breakParagraphs(text)
+        createSaveFile(path.join(__dirname, '../files', result))
     } catch (error) {
         errorHandler(error, text)
     }
 
 })
+
+async function createSaveFile(wordsList, pathToSave) {
+    const newFile = `${pathToSave}/output.txt`
+    const wordsText = JSON.stringify(wordsList)
+    try {
+        await fs.promises.writeFile(newFile, wordsText)
+        console.log('File create')
+    } catch (error) {
+        throw error
+    }
+}
 
 // const filesPath = process.argv // arguments vector
